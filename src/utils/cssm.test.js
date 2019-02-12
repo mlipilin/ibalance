d88fee15b752ce4b9a9f28fe5c1185895d991361
Mikhail Lipilin <lipilin.m@gmail.com>
2019-02-12 18:26:33 +0300

    setup jest

--- src/utils/cssm.test.js
+++ src/utils/cssm.test.js
@@ -2,11 +2,11 @@ import cssm from './cssm';
 
 describe('src/utils/cssm.js', () => {
     it('', () => {
-        const styles = {
+        const stylesMap = {
             a: 'a_hash',
             b: 'b_hash',
             c: 'c_hash',
         };
-        expect(cssm(styles, 'a b d', 'e')).toBe('a_hash b_hash e_hash');
+        expect(cssm(stylesMap, 'a b d', 'e')).toBe('a_hash b_hash e');
     });
 });
