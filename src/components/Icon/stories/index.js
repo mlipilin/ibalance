b374b575a47f30a2ad8d9d385b0479a691682799
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2019-04-01 17:04:45 +0300

    new format for icons

--- src/components/Icon/stories/index.js
+++ src/components/Icon/stories/index.js
@@ -1,52 +1,10 @@
-import React from 'react';
-import { icons } from 'feather-icons';
-
 import { storiesOf } from '@storybook/react';
 
-import Icon from '../index';
-
+import stories from './stories';
 import notes from '../readme.md';
 
-import './styles.css';
+let componentStories = storiesOf('Icon');
 
-const SIZES = ['xs', 's', 'm', 'l', 'xl', 'xxl'];
-
-let stories = storiesOf('Icon');
-
-stories.add(
-    'Size = 24px (default)',
-    () => (
-        <div className="Story__Icon-Container ">
-            {Object.keys(icons).map(name => (
-                <Icon key={name} name={name} title={name} />
-            ))}
-        </div>
-    ),
-    { notes },
-);
-
-SIZES.forEach(size => {
-    stories.add(
-        `Size = ${size.toUpperCase()}`,
-        () => (
-            <div className="Story__Icon-Container">
-                {Object.keys(icons).map(name => (
-                    <Icon key={name} name={name} size={size} title={name} />
-                ))}
-            </div>
-        ),
-        { notes },
-    );
+Object.keys(stories).forEach(type => {
+    componentStories.add(type, () => stories[type].map(s => s.component), { notes });
 });
-
-stories.add(
-    'Colored',
-    () => (
-        <div className="Story__Icon-Container ">
-            {Object.keys(icons).map(name => (
-                <Icon color="#007bff" key={name} name={name} title={name} />
-            ))}
-        </div>
-    ),
-    { notes },
-);
