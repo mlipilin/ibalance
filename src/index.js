48f206044e5702a9de34dc4b57eb149a62b04c76
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2019-04-08 18:17:27 +0300

    hotfix: export notification component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import Button from './components/Button';
 import Icon from './components/Icon';
 import Input from './components/Input';
+import { Notification, Wrapper as NotificationWrapper } from './components/Notification';
 import Spin from './components/Spin';
 
 import ThemeProvider, { useTheme } from './theme-provider';
 
 import './themes/default/index.less';
 
-export { Button, Icon, Input, Spin, ThemeProvider, useTheme };
-export default { Button, Icon, Input, Spin, ThemeProvider, useTheme };
+export { Button, Icon, Input, Notification, NotificationWrapper, Spin, ThemeProvider, useTheme };
+export default {
+    Button,
+    Icon,
+    Input,
+    Notification,
+    NotificationWrapper,
+    Spin,
+    ThemeProvider,
+    useTheme,
+};
